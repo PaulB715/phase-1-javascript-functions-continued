7350f8044528f9632b1f266b489b18c07d6bf5a4
Paul Brown <paulbrown389@gmail.com>
2023-12-17 21:32:28 -0500

    Function Cont

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(activity) {
+    if (arguments.length === 0) {
+      return "This Saturday, I want to roller-skate!";
+    } else {
+      return "This Saturday, I want to " + activity + "!";
+    }
+  }
+  function mondayWork(activity){
+    if (arguments.length === 0) {
+        return "This Monday, I will go to the office."
+    } else {
+        return "This Monday, I will " + activity + "."
+    }
+  }
+  function wrapAdjective(character) {
+    return function(adjective) {
+      return `You are ${character}${adjective}${character}!`;
+    };
+  }
